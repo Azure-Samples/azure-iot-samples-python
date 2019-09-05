aabb34f3e8716820622999b0baf0485a7ac8a2f3
Carter Tinney <catinney@microsoft.com>
2019-09-05 13:49:49 -0700

    Changed Python quickstarts and tutorials to use V2 SDK (#15)
    * Changed quickstarts to use V2 SDK
    
    Edge tutorial compatibility with V2 SDK
    
    * fixed message bug

--- iot-hub/Quickstarts/simulated-device-2/SimulatedDevice.py
+++ iot-hub/Quickstarts/simulated-device-2/SimulatedDevice.py
@@ -3,60 +3,58 @@
 
 import random
 import time
-import sys
+import threading
 
 # Using the Python Device SDK for IoT Hub:
 #   https://github.com/Azure/azure-iot-sdk-python
 # The sample connects to a device-specific MQTT endpoint on your IoT Hub.
-import iothub_client
-# pylint: disable=E0611
-from iothub_client import IoTHubClient, IoTHubClientError, IoTHubTransportProvider, IoTHubClientResult
-from iothub_client import IoTHubMessage, IoTHubMessageDispositionResult, IoTHubError, DeviceMethodReturnValue
+from azure.iot.device import IoTHubDeviceClient, Message, MethodResponse
 
 # The device connection string to authenticate the device with your IoT hub.
 # Using the Azure CLI:
 # az iot hub device-identity show-connection-string --hub-name {YourIoTHubName} --device-id MyNodeDevice --output table
 CONNECTION_STRING = "{Your IoT hub device connection string}"
 
-# Using the MQTT protocol.
-PROTOCOL = IoTHubTransportProvider.MQTT
-MESSAGE_TIMEOUT = 10000
-
 # Define the JSON message to send to IoT Hub.
 TEMPERATURE = 20.0
 HUMIDITY = 60
-MSG_TXT = "{\"temperature\": %.2f,\"humidity\": %.2f}"
+MSG_TXT = '{{"temperature": {temperature},"humidity": {humidity}}}'
 
 INTERVAL = 1
 
-def send_confirmation_callback(message, result, user_context):
-    print ( "IoT Hub responded to message with status: %s" % (result) )
-
 def iothub_client_init():
     # Create an IoT Hub client
-    client = IoTHubClient(CONNECTION_STRING, PROTOCOL)
+    client = IoTHubDeviceClient.create_from_connection_string(CONNECTION_STRING)
     return client
 
-# Handle direct method calls from IoT Hub
-def device_method_callback(method_name, payload, user_context):
+
+def device_method_listener(device_client):
     global INTERVAL
-    print ( "\nMethod callback called with:\nmethodName = %s\npayload = %s" % (method_name, payload) )
-    device_method_return_value = DeviceMethodReturnValue()
-    if method_name == "SetTelemetryInterval":
-        try:
-            INTERVAL = int(payload)
-            # Build and send the acknowledgment.
-            device_method_return_value.response = "{ \"Response\": \"Executed direct method %s\" }" % method_name
-            device_method_return_value.status = 200
-        except ValueError:
-            # Build and send an error response.
-            device_method_return_value.response = "{ \"Response\": \"Invalid parameter\" }"
-            device_method_return_value.status = 400
-    else:
-        # Build and send an error response.
-        device_method_return_value.response = "{ \"Response\": \"Direct method not defined: %s\" }" % method_name
-        device_method_return_value.status = 404
-    return device_method_return_value
+    while True:
+        method_request = device_client.receive_method_request()
+        print (
+            "\nMethod callback called with:\nmethodName = {method_name}\npayload = {payload}".format(
+                method_name=method_request.name,
+                payload=method_request.payload
+            )
+        )
+        if method_request.name == "SetTelemetryInterval":
+            try:
+                INTERVAL = int(method_request.payload)
+            except ValueError:
+                response_payload = {"Response": "Invalid parameter"}
+                response_status = 400
+            else:
+                response_payload = {"Response": "Executed direct method {}".format(method_request.name)}
+                response_status = 200
+        else:
+            response_payload = {"Response": "Direct method {} not defined".format(method_request.name)}
+            response_status = 404
+
+        method_response = MethodResponse(method_request.request_id, response_status, payload=response_payload)
+        device_client.send_method_response(method_response)
+
+
 
 def iothub_client_telemetry_sample_run():
 
@@ -64,33 +62,31 @@ def iothub_client_telemetry_sample_run():
         client = iothub_client_init()
         print ( "IoT Hub device sending periodic messages, press Ctrl-C to exit" )
 
-        # Set up the callback method for direct method calls from the hub.
-        client.set_device_method_callback(
-            device_method_callback, None)
+        # Start a thread to listen 
+        device_method_thread = threading.Thread(target=device_method_listener, args=(client,))
+        device_method_thread.daemon = True
+        device_method_thread.start()
 
         while True:
             # Build the message with simulated telemetry values.
             temperature = TEMPERATURE + (random.random() * 15)
             humidity = HUMIDITY + (random.random() * 20)
-            msg_txt_formatted = MSG_TXT % (temperature, humidity)
-            message = IoTHubMessage(msg_txt_formatted)
+            msg_txt_formatted = MSG_TXT.format(temperature=temperature, humidity=humidity)
+            message = Message(msg_txt_formatted)
 
             # Add a custom application property to the message.
             # An IoT hub can filter on these properties without access to the message body.
-            prop_map = message.properties()
             if temperature > 30:
-              prop_map.add("temperatureAlert", "true")
+              message.custom_properties["temperatureAlert"] = "true"
             else:
-              prop_map.add("temperatureAlert", "false")
+              message.custom_properties["temperatureAlert"] = "false"
 
             # Send the message.
-            print( "Sending message: %s" % message.get_string() )
-            client.send_event_async(message, send_confirmation_callback, None)
+            print( "Sending message: {}".format(message) )
+            client.send_message(message)
+            print( "Message sent" )
             time.sleep(INTERVAL)
 
-    except IoTHubError as iothub_error:
-        print ( "Unexpected error %s from IoTHub" % iothub_error )
-        return
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
 

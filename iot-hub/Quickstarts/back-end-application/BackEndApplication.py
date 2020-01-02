5fdad7dff79f6556f2c1926929ce2e294daf53a1
jebrando <jebrando@microsoft.com>
2020-01-02 14:04:48 -0800

    Update BackEndApplication for the new python v2 release

--- iot-hub/Quickstarts/back-end-application/BackEndApplication.py
+++ iot-hub/Quickstarts/back-end-application/BackEndApplication.py
@@ -1,34 +1,37 @@
-import sys
-
 # Copyright (c) Microsoft. All rights reserved.
 # Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 # Using the Python Device SDK for IoT Hub:
 #   https://github.com/Azure/azure-iot-sdk-python
 # The sample connects to a device-specific MQTT endpoint on your IoT Hub.
-import iothub_service_client
+
 # pylint: disable=E0611
-from iothub_service_client import IoTHubDeviceMethod, IoTHubError
-from builtins import input
+import sys
+import os
+from azure.iot.hub import IoTHubRegistryManager
+from azure.iot.hub.protocol.models import CloudToDeviceMethod
 
 # The service connection string to authenticate with your IoT hub.
 # Using the Azure CLI:
 # az iot hub show-connection-string --hub-name {your iot hub name} --policy-name service
 CONNECTION_STRING = "{Your IoT hub service connection string}"
-DEVICE_ID = "MyPythonDevice"
+DEVICE_ID = "{Your IoTHub Device ID}"
 
 # Details of the direct method to call.
 METHOD_NAME = "SetTelemetryInterval"
 METHOD_PAYLOAD = "10"
-TIMEOUT = 60
 
 def iothub_devicemethod_sample_run():
     try:
         # Connect to your hub.
-        iothub_device_method = IoTHubDeviceMethod(CONNECTION_STRING)
+        registry_manager = IoTHubRegistryManager(CONNECTION_STRING)
 
         # Call the direct method.
-        response = iothub_device_method.invoke(DEVICE_ID, METHOD_NAME, METHOD_PAYLOAD, TIMEOUT)
+        deviceMethod = CloudToDeviceMethod(
+            method_name = METHOD_NAME,
+            payload = METHOD_PAYLOAD
+        )
+        response = registry_manager.invoke_device_method(DEVICE_ID, deviceMethod)
 
         print ( "" )
         print ( "Device Method called" )
@@ -40,13 +43,10 @@ def iothub_devicemethod_sample_run():
 
         input("Press Enter to continue...\n")
 
-    except IoTHubError as iothub_error:
-        print ( "" )
-        print ( "Unexpected error {0}".format(iothub_error) )
-        return
+    except Exception as ex:
+        print("Unexpected error {0}".format(ex))
     except KeyboardInterrupt:
-        print ( "" )
-        print ( "IoTHubDeviceMethod sample stopped" )
+        print("iothub_registry_manager_sample stopped")
 
 if __name__ == '__main__':
     print ( "IoT Hub Python quickstart #2..." )
